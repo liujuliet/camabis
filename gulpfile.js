2c9fd86332332f589bbbcaf1509f4dde199d47ea
melodyk <melody.kuo22@gmail.com>
2016-03-19 20:05:30 -0400

    Updated gulpfile
    * Added dist directory
    * Added task for 'gulp build'
    * Switched to less from css

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,52 @@
 var gulp = require ('gulp'),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    del = require('del'),
+    less = require('gulp-less'),
+    uglify = require('gulp-uglify');
+
+var paths = {
+    dist: 'dist',
+    html: ['src/**/*.html'],
+    js: ['src/**/*.js'],
+    less: ['src/**/*.less']
+}
+
+gulp.task('clean', function() {
+    del.sync(paths.dist);
+});
 
 gulp.task('connect', function() {
     connect.server({
         port: 8000,
         livereload:true,
-        root: ['src']
+        root: [paths.dist]
     });
 });
 
 gulp.task('html', function () {
-    gulp.src('src/*.html')
+    gulp.src(paths.html)
+        .pipe(gulp.dest(paths.dist))
+        .pipe(connect.reload());
+});
+
+gulp.task('scripts', function() {
+    gulp.src(paths.js)
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.dist))
+        .pipe(connect.reload());
+});
+
+gulp.task('styles', function() {
+    gulp.src(paths.less)
+        .pipe(less())
+        .pipe(gulp.dest(paths.dist))
         .pipe(connect.reload());
 });
 
 gulp.task('watch', function() {
-    gulp.watch('src/*.html', ['html']);
+    gulp.watch(paths.html, ['html']);
+    gulp.watch(paths.less, ['styles'])
 });
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', ['html', 'styles', 'scripts', 'connect', 'watch']);
+gulp.task('build', ['clean', 'html', 'styles', 'scripts']);
