1d50230642bad4ba1c716d37222dbfb4d356ba34
Juliet Liu <julietliu94@gmail.com>
2016-03-27 00:49:14 -0400

    Adding the dist folder

---
+++ dist/index.js
@@ -0,0 +1,32 @@
+var pg = require('pg');
+var http = require('http');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/dist'));
+
+// app.get('/', function(request, response) {
+//   response.render('index.html');
+// });
+
+// app.get('/db', function (request, response) {
+//   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+//     client.query('SELECT * FROM test_table', function(err, result) {
+//       done();
+//       if (err)
+//        { console.error(err); response.send("Error " + err); }
+//       else
+//        { response.render('/db', {results: result.rows} ); }
+//     });
+//   });
+// });
+
+app.get('/', function(req, res) {
+  res.sendFile('./index.html');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
