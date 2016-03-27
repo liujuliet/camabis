621a5c2b241760f4bc0423112b5f6e3fa15833ad
Juliet Liu <julietliu94@gmail.com>
2016-03-26 21:59:33 -0400

    Added postgres db

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var pg = require('pg');
 var express = require('express');
 var app = express();
 
@@ -17,4 +18,14 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
