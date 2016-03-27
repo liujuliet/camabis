b85f3d50d8116cd7ab8e3aee3cefaabc45f8b368
Juliet Liu <julietliu94@gmail.com>
2016-03-27 15:23:07 -0400

    Fixed databse url

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,5 +1,5 @@
 angular.module('myApp.config', [])
-.constant('databaseUrl', "https://camabis.herokuapp.com/app/")
+.constant('databaseUrl', "https://camabis.herokuapp.com/api")
 .constant('serverUrl', "http://localhost:8000/");
 
 
