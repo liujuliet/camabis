49aef5365d62f65500b8dc5ecae98de60e38697f
Juliet Liu <julietliu94@gmail.com>
2016-03-28 11:51:31 -0400

    changed app back to router post

--- src/Home/Home.controller.js
+++ src/Home/Home.controller.js
@@ -86,29 +86,6 @@
             });
         }
 
-        function uploadButtonHandler(file) {
-            // DatabaseFactory.postPhoto(file);
-
-            var req = {
-                method: 'POST',
-                url: '/api/photo',
-                headers: {
-                    'enctype': 'multipart/form-data',
-                    // 'Content-type': 'application/x-www-form-urlencoded',
-                    'Access-Control-Allow-Origin': '*'
-                },
-                data: file
-            };
-
-            $http(req).then(function success (res) {
-                console.log('SUCCESSSS inside button handler');
-                console.log(res);
-            }, function err (res) {
-                console.log('ERROR');
-                console.log(res);
-            });
-        }
-
         function uploadPhoto(file) {
             var req = {
                 method: 'POST',
